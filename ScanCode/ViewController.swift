362e093618ea44bae4ccd79c1aaf5d3c98234ceb
hekai <th-011@hekaidembp.lan>
2018-03-29 14:31:49 +0800

    Initial Commit

---
+++ ScanCode/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ScanCode
+//
+//  Created by TH-011 on 2018/3/29.
+//  Copyright © 2018年 TH-011. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
